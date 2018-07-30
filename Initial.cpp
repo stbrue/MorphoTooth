6c5441a9166a324d3381d391921200c1f2b9db71
STEFANIESPC\Stefanie <stefanie.bruehlmeier@gmail.com>
2018-07-30 14:03:07 +0200

    + getTotalNumberOfCells + getNumberOfInSimulationCells

---
+++ Initial.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Stefanie on 30.07.2018.
+//
+
+#include "Initial.h"
---
+++ Initial.h
@@ -0,0 +1,14 @@
+//
+// Created by Stefanie on 30.07.2018.
+//
+
+#ifndef TOOTHMAKER_INITIAL_H
+#define TOOTHMAKER_INITIAL_H
+
+
+class Initial {
+
+};
+
+
+#endif //TOOTHMAKER_INITIAL_H
